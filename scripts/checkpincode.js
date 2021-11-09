450e62e010b1158ed06dd82ac8221b377540ccf7
vasukomuravelli <vasukomuravelli96@gmail.com>
2021-11-09 16:10:22 +0530

    Delete checkpincode.js

--- scripts/checkpincode.js
+++
@@ -1,7 +0,0 @@
-async function checkpincode(){
-    let input = document.querySelector('.del > div input').value;
-    let res = await fetch(`https://api.postalpincode.in/pincode/${input}`);
-    let address = await res.json();
-    console.log(address);
-}
-export {checkpincode}
