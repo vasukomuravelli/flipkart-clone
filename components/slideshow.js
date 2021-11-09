b3ec42dedf5c393fcdfaf643400cf242a5d1d23b
vasukomuravelli <vasukomuravelli96@gmail.com>
2021-11-09 10:55:08 +0530

    Create slideshow.js

---
+++ components/slideshow.js
@@ -0,0 +1,27 @@
+function slideshow()
+{
+    return `
+
+    <div class="mySlides mySlides--visible">
+        <img src="https://web.archive.org/web/20211023191726/https://rukminim1.flixcart.com/flap/1688/280/image/8091031a2235c8ff.jpg?q=50"/>   
+    </div>
+
+    <div class="mySlides">
+        <img src = "https://web.archive.org/web/20211023191736/https://rukminim1.flixcart.com/flap/1688/280/image/3b88d7395b9a1370.jpeg?q=50"/>    
+    </div>
+
+    <div class="mySlides">
+        <img src = "https://web.archive.org/web/20211023191735/https://rukminim1.flixcart.com/flap/1688/280/image/ff938f15fd1feb73.jpg?q=50"/>        
+    </div>
+
+    <div class="mySlides">
+        <img src = "https://web.archive.org/web/20211102051615/https://rukminim1.flixcart.com/flap/1688/280/image/3fb4a8cf5a8ee74c.jpg?q=50"/>        
+    </div>
+    <div class= "actions">
+        <a id="prev">&#10094;</a>
+        <a id="next">&#10095;</a>
+    </div>`    
+    
+}
+
+export default slideshow;
