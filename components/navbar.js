d9e28a59bb2cc64b1f5821ef9a6d10260fc0efbe
vasukomuravelli <vasukomuravelli96@gmail.com>
2021-11-09 16:09:28 +0530

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,7 @@ function navbar()
 {
     return `
 <div>
-    <div class="logo">
+    <a href = "index.html"><div class="logo">
         <img src="https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png">
         <div class="caption">
             <div class="flex">
@@ -11,17 +11,30 @@ function navbar()
             </div>
             <img src="https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png">
         </div>
-    </div>
+    </div></a>
     <div>
         <input type="text" placeholder="Search for products,brand and more" id="searchbox">
         <span class="material-icons" id = "search_icon">search</span>
     </div>
 </div>
 <div>
-    <p class = "login">Login</p>
-    <div class="flex">
-        <p class = "text" id= "more">More</p>
-        <span class="material-icons micon">arrow_drop_down</span>
+    <div class = "login_div">
+        <p class = "login">Login</p>
+        <ul class= "login_dropdown">
+            <li><b>New Customer? <a>Signup</a></b></li>
+            <li><span class="material-icons">account_circle</span> My Profile</li>
+            <li>Flipkart Plus Zone</li>
+            <li><span class="material-icons">list_alt</span> Orders</li>
+            <li><span class="material-icons">favorite</span> Wishlist</li>
+            <li><span class="material-icons">confirmation_number</span> Rewards</li>
+            <li><span class="material-icons">account_balance_wallet</span> Gift Cards</li>
+        </ul>
+    </div>
+    <div class="more">
+        <div class="flex">
+            <p class = "text" id= "more">More</p>
+            <span class="material-icons micon">arrow_drop_down</span>
+        </div>
         <ul class = "dropdown">
             <li><span class="material-icons">notifications</span> Notification Preferances</li>
             <li><span class="material-icons">shopping_bag</span> Sell on Flipkart</li>
