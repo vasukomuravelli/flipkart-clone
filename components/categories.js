ccd13aa84325e8bb319011623517953292187b19
vasukomuravelli <vasukomuravelli96@gmail.com>
2021-11-12 09:31:47 +0530

    Update categories.js

--- components/categories.js
+++ components/categories.js
@@ -1,42 +1,42 @@
 function categories(){
     return`
     <div>
-        <div>
+        <a><div>
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100"/>
             <p>Top Offers</p>
-        </div>
-        <div>
+        </div></a>
+        <a href = "expect_soon.html"><div>
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100"/>
             <p>Grocery</p>
-        </div>
-        <div>
+        </div></a>
+        <a href = "expect_soon.html"><div>
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100"/>
             <p>Mobile</p>
-        </div>
-        <div>
+        </div></a>
+        <a><div>
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/82b3ca5fb2301045.png?q=100"/>
             <p>Fashion</p>
-        </div>
-        <div>
+        </div></a>
+        <a href = "expect_soon.html"><div>
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100"/>
             <p>Electronics</p>
-        </div>
-        <div>
+        </div></a>
+        <a><div>
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/ab7e2b022a4587dd.jpg?q=100"/>
             <p>Home</p>
-        </div>
-        <div>
+        </div></a>
+        <a><div>
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100"/>
             <p>Appliances</p>
-        </div>
-        <div>
+        </div></a>
+        <a><div>
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100"/>
             <p>Travel</p>
-        </div>
-        <div>
+        </div></a>
+        <a><div>
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100"/>
             <p>Beauty, Toys & More</p>
-        </div>
+        </div></a>
     </div>`;
 }
 
