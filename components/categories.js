3d049c323d7e149f50d49fd17d2dab2040c086bd
vasukomuravelli <vasukomuravelli96@gmail.com>
2021-11-13 19:44:42 +0530

    Update categories.js

--- components/categories.js
+++ components/categories.js
@@ -1,7 +1,7 @@
 function categories(){
     return`
     <div>
-        <a><div>
+        <a href = "expect_soon.html"><div>
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100"/>
             <p>Top Offers</p>
         </div></a>
@@ -13,7 +13,7 @@ function categories(){
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100"/>
             <p>Mobile</p>
         </div></a>
-        <a><div>
+        <a href = "filter.html"><div>
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/82b3ca5fb2301045.png?q=100"/>
             <p>Fashion</p>
         </div></a>
@@ -21,19 +21,19 @@ function categories(){
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100"/>
             <p>Electronics</p>
         </div></a>
-        <a><div>
+        <a href = "filter.html"><div>
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/ab7e2b022a4587dd.jpg?q=100"/>
             <p>Home</p>
         </div></a>
-        <a><div>
+        <a href = "expect_soon.html"><div>
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100"/>
             <p>Appliances</p>
         </div></a>
-        <a><div>
+        <a href = "filter.html"><div>
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100"/>
             <p>Travel</p>
         </div></a>
-        <a><div>
+        <a href = "filter.html"><div>
             <img src="https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100"/>
             <p>Beauty, Toys & More</p>
         </div></a>
