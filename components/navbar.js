66e50c61a613279b8efa33dc6f61f9742a5b3a8d
vasukomuravelli <vasukomuravelli96@gmail.com>
2021-11-13 19:44:49 +0530

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -24,11 +24,11 @@ function navbar()
     <div class = "login_div">
         <p class = "login">Login</p>
         <ul class= "login_dropdown">
-            <li><b>New Customer? <a>Signup</a></b></li>
+            <li><b>New Customer? <a href = "./sample.html" class= "signup">Signup</a></b></li>
             <li><span class="material-icons">account_circle</span> My Profile</li>
             <li><span class="material-icons">add</span> Flipkart Plus Zone</li>
             <li><span class="material-icons">list_alt</span> Orders</li>
-            <li><span class="material-icons">favorite</span> Wishlist</li>
+            <li><a href = "./wishlist.html"><span class="material-icons">favorite</span> Wishlist</a></li>
             <li><span class="material-icons">confirmation_number</span> Rewards</li>
             <li><span class="material-icons">account_balance_wallet</span> Gift Cards</li>
         </ul>
@@ -45,10 +45,10 @@ function navbar()
             <li><span class="material-icons">moving</span> Advertise</li>
             <li><span class="material-icons">get_app</span> Download App</li>
     </div>
-    <div class="flex">
+    <a href = "cart.html"><div class="flex">
         <span class="material-icons">shopping_cart</span>
         <p class = "text">Cart</p>
-    </div>
+    </div></a>
 </div>`;
 }
 
